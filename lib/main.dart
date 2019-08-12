b14952d3073e6fb39f08c7c775ccd1b5c4ead299
saurabh <parmarsaurabh886@gmail.com>
2019-08-13 00:18:01 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:devfest/utils/devfest.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> main() async{
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
+
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]
+  );
+
+  Devfest.preferences = await SharedPreferences.getInstance();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google Devfest',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Text("main"),
+    );
+  }
+}
